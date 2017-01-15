3475eb85847215e6605146942918ea31cb04fed8
47_TT <miraculousjade@gmail.com>
2017-01-16 01:23:56 +0900

    コメントをつける

--- app/main.py
+++ app/main.py
@@ -1,39 +1,46 @@
 ﻿# coding: utf-8
-# エンコードはutf-8
 
 import sys
 import urllib
 import urllib2
 import xml.etree.ElementTree as ET
-from collections import OrderedDict
-import json
 
 def main(argv):
-  maxtitle = ""
-  maxnumber = -1
+    #最大件数取得用の変数を初期化
+    maxtitle = ""
+    maxnumber = -1
 
-  for v in argv:
-    urlprefix = 'http://54.92.123.84/search?'
-    query = [
-      ('ackey', '869388c0968ae503614699f99e09d960f9ad3e12'),
-      ('q', 'Body:' + urllib.quote(v)),
-    ]
+    for v in argv:
+        #キーワードと検索期間からクエリを作成
+        urlprefix = 'http://54.92.123.84/search?'
+        query = [
+            ('ackey', '869388c0968ae503614699f99e09d960f9ad3e12'),
+            ('q', 'Body:' + urllib.quote(v)),
+        ]
 
-    url = urlprefix
-    for item in query:
-      url += item[0] + "=" + item[1] + "&"
-    url = url[:-1]
+        #URLの形に整形
+        url = urlprefix
+        for item in query:
+            url += item[0] + "=" + item[1] + "&"
+        url = url[:-1]
 
-    response = urllib2.urlopen(url)
-    resdata = response.read()
+        #レスポンスを取得
+        response = urllib2.urlopen(url)
+        resdata = response.read()
 
-    root = ET.fromstring(resdata)
-    result = root.find('.//result')
-    number = int(result.get('numFound'))
+        #XMLを解析して件数を取得
+        root = ET.fromstring(resdata)
+        result = root.find('.//result')
+        number = int(result.get('numFound'))
 
-    if (number > maxnumber):
-      maxnumber = number
-      maxtitle = v
+        #最大件数の項目を更新
+        if (number > maxnumber):
+            maxnumber = number
+            maxtitle = v
 
-  string = '{"name":"' + maxtitle + '","count":' + str(maxnumber) + '}'
-  print(string)
+
+    #出力を整形
+    string = '{"name":"' + maxtitle + '","count":' + str(maxnumber) + '}'
+
+    #出力
+    print(string)
--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 ﻿# coding: utf-8
-# エンコードはutf-8
 
 import sys
 from app.main import main
 
+#デフォルトのエンコーディングをUTF-8に設定
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+#実行
 main(sys.argv[1:])
