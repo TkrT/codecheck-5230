a6d33eedda70b35a47da8b1955fa824fac5bbfae
47_TT <miraculousjade@gmail.com>
2017-01-18 09:56:12 +0900

    python2に戻す

--- index.py
+++ index.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python3
+# coding: utf-8
+
 import sys
 from app.main import main
 
+#デフォルトのエンコーディングをUTF-8に設定
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#実行
 main(sys.argv[1:])
