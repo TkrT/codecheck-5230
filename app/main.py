a3ba8aa77a932009b38160bd053bde1fa3d55aed
47_TT <miraculousjade@gmail.com>
2017-01-20 21:42:31 +0900

    グローバル変数を無くす

--- app/main.py
+++ app/main.py
@@ -14,58 +14,53 @@ def get(url):
 
 #記事件数の取得
 @asyncio.coroutine
-def getArticleNumber(keyword):
-    global AsahiURLPrefix
-    global maxTitle
-    global maxNumber
-
+def getMaximumArticleNumber(keyword, prefix, maxTitle, maxNumber):
     #URLの作成と読み込み
-    query = 'q=Body%3A' + urllib.parse.quote(keyword.encode('utf-8'))
-    url = AsahiURLPrefix + query
+    q = "Body:" + keyword 
+    url = prefix + 'q=' + urllib.parse.quote(q.encode('utf-8'))
     page = yield from get(url)
 
     #XMLを解析して件数を取得
-    root = ET.fromstring(page)
-    result = root.find('.//result')
-    number = int(result.get('numFound'))
+    number = analyzeAsahiXML(page)
 
     #最大件数の項目を更新
-    if (number > maxNumber):
-        maxNumber = number
-        maxTitle = keyword
+    if (number > maxNumber[0]):
+        maxNumber[0] = number
+        maxTitle[0] = keyword
+
+def analyzeAsahiXML(page):
+    #件数を取得
+    root = ET.fromstring(page)
+    result = root.find('.//result')
+    return (int(result.get('numFound')))
 
 def main(argv):
-    #global変数の定義
-    global AsahiURLPrefix
-    global maxTitle
-    global maxNumber
-
     #キーワードリストを作成
     keywordNumber = len(argv)
     Keywords = []
     for i in range(0, keywordNumber):
         Keywords.append(os.fsencode(argv[i]).decode('utf-8'))
 
-    #最大件数取得用変数を初期化
-    maxTitle = ""
-    maxNumber = -1
-
     #記事検索用のプレフィックスを作成
-    AsahiURLPrefix = 'http://54.92.123.84/search?'
+    prefix = 'http://54.92.123.84/search?'
     query = [
         ('ackey', '869388c0968ae503614699f99e09d960f9ad3e12'),
         ('rows', '1'),
     ]
     for item in query:
-        AsahiURLPrefix += item[0] + "=" + item[1] + "&"
+        prefix += item[0] + "=" + item[1] + "&"
+
+    #最大件数取得用変数を初期化
+    maxTitle = [""]
+    maxNumber = [-1]
 
     #記事検索
     loop = asyncio.get_event_loop()
-    f = asyncio.wait([getArticleNumber(d) for d in Keywords])
+    f = asyncio.wait([getMaximumArticleNumber(v, prefix, maxTitle, maxNumber) for v in Keywords])
     loop.run_until_complete(f)
 
     #出力を整形
-    string = '{"name":"' + maxTitle + '","count":' + str(maxNumber) + '}'
+    string = '{"name":"' + maxTitle[0] + '","count":' + str(maxNumber[0]) + '}'
 
     #出力
     print(string)
